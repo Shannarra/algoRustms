37f45a07c220697fa94ea14a259ba11d2f6a0631
Shannarra <petarangelov15@gmail.com>
2021-04-09 22:30:12 +0200

    Added command-line reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read, BufRead};
+
+///
+/// ```rust
+/// assert_eq!(read_contents(".gitignore"), "\".gitignore\" file contents: \n/target\n")
+/// ```
+fn read_contents(file: &str) -> String {
+    match std::fs::read_to_string(file) {
+        Ok(content) => format!("\"{}\" file contents: \n{}", file, content),
+        Err(e) => panic!(e)
+    }
+}
+
+fn rust_in(result: &mut String) {
+   for line in io::stdin().lock().lines() {
+       return result.push_str(&*line.unwrap())
+   }
+}
+
+fn main() -> std::io::Result<()>{
+
+    let mut input = String::new();
+
+    rust_in(  &mut input);
+    println!("{}", read_contents(&input));
+    assert_eq!(read_contents(".gitignore"), "\".gitignore\" file contents: \n/target\n");
+    Ok(())
 }
